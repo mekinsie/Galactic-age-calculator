9a56cbe2026887965f46d0dd9b2513027c4025ac
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-05 10:11:01 -0800

    Write second test.

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -10,9 +10,12 @@ describe('PlanetAge', () => {
     expect(planetAge.mars).toEqual(yourAge * 1.88);
     expect(planetAge.jupiter).toEqual(yourAge * 11.86);
   });
+
+  test('Should correctly calculate a user age based on what planet they are on.');
+  let yourAge = 30;
+  expect(planetAge.venus).toEqual();
 });
 
 
 
-// test('Should correctly calculate a user age based on what planet they are on.');
 
