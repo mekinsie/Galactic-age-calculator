9cd236be9d408c4aec4d27004509849a9486d0d9
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-05 13:38:27 -0800

    Add life expectancy facts in comments.

--- src/js/age.js
+++ src/js/age.js
@@ -1,24 +1,34 @@
 export default class UserDemogaphic {
   constructor(earthAge, activityLevel) {
     this.earthAge = earthAge;
+    this.planetAge = planetAge;
     this.activityLevel = activityLevel;
+    earthLifeExpect = 73;b
   }
   planetAgeCalc (planet) {
     if (planet === "Mercury") {
-    return (this.earthAge * 0.24);
+      let planetAge =  (this.earthAge * 0.24);
+      return planetAge;
     }
     if (planet === "Venus") {
-      return (this.earthAge * 0.62);
+      let planetAge = (this.earthAge * 0.62);
+      return planetAge;
       }
     if (planet === "Mars") {
-      return (this.earthAge * 1.88);
+      let planetAge = (this.earthAge * 1.88);
+      return planetAge;
       }
     if (planet === "Jupiter") {
-      return (this.earthAge * 11.86);
+      let planetAge = (this.earthAge * 11.86);
+      return planetAge;
       }
   }
+  //73 years is the average earth life expectancy for a human.
+  // Exercise increases life expectancy from 0.43 years to 6.9 years. https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3395188/The added years of life for each activity level are based on this, but have no factual meaning.
   planetLifeExpect () {
-    return this.activityLevel;
+    if (this.activityLevel === "Sedentary" && planet=== "Mercury") {
+      return (((73-3.7)*0.24)) - planetAge;
+    }
   }
 }
 
