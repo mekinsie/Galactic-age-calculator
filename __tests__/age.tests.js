272ccfeb3258eff57be91e5f4e2a552e46f567ad
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-05 09:40:25 -0800

    Write first test.

---
+++ __tests__/age.tests.js
@@ -0,0 +1,16 @@
+import { TestScheduler } from 'jest';
+import PlanetAge from '../src/js/age.js'
+
+describe('PlanetAge', () => {
+  let planetAge = new PlanetAge(yourAge);
+  test('Should correctly create a PlanetAge object with planet and their formula values');
+  expect(planetAge.mercury).toEqual(yourAge * 0.24);
+  expect(planetAge.venus).toEqual(yourAge * 0.62);
+  expect(planetAge.mars).toEqual(yourAge * 1.88);
+  expect(planetAge.jupiter).toEqual(yourAge * 11.86);
+});
+
+
+
+// test('Should correctly calculate a user age based on what planet they are on.');
+
