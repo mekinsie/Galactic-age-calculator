c918e4160c225ab0208d7957d721eec3d4ded211
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-06 17:50:44 -0800

    Add appended message when submit button is clicked.

--- src/main.js
+++ src/main.js
@@ -7,16 +7,18 @@ import UserDemographic from './js/age.js';
 $(document).ready(function(){
   $('form#age-activity-form').submit(function(event){
     event.preventDefault();
-    $("div#results").empty();
     let earthAge = $("#age").val();
     let planet = $("#planet option:selected").val();
     let activityLevel = $("#activity-level option:selected").val();
-
+    
     let user = new UserDemographic(earthAge, activityLevel);
     let planetAge = user.planetAgeCalc(planet);
     let lifeExpect = user.determineLifeExpect();
     let remainLife = user.calcRemainLife(planet, lifeExpect, planetAge);
-
-    $("div#results").append(`On ${planet}, you are ${planetAge} years old. You have ${remainLife} years left of living.`);
+    $("div#results").empty();
+    $("div#results").append(`On ${planet}, you are ${planetAge.toFixed(1)} years old.`);
+    $("span#years-remaining").empty();
+    $("span#years-remaining").append(remainLife);
+    $("div#years-div").show();
   });
 });
